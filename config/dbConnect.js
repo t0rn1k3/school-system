75150e4e0a1268ac63ada4a3477af33151a91cd8
t0rn1k3 <tornikeburjanadze0@gmail.com>
2026-01-27 17:23:25 +0400

    create environment file and save there mongo url for safe and use variable in dbConnect file instead

---
+++ config/dbConnect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const dbConnect = async () => {
+  try {
+    console.log(process.env);
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("DB Conectio Failed", error.message);
+  }
+};
+
+dbConnect();
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const http = require("http");
+require("./config/dbConnect");
 const app = require("./app/app");
 
 const PORT = process.env.PORT || 2020;
