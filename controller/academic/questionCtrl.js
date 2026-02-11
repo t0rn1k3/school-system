2b3952ed3060b61535cf16e33e07d8e1bc7cc9df
t0rn1k3 <tornikeburjanadze0@gmail.com>
2026-02-11 14:27:45 +0400

    add create queatin controller woth careteQuesrion handler

---
+++ controller/academic/questionCtrl.js
@@ -0,0 +1,48 @@
+const AsyncHandler = require("express-async-handler");
+const Question = require("../../model/Academic/Question");
+const Exam = require("../../model/Academic/Exam");
+const Teacher = require("../../model/Staff/Teacher");
+const isTeacherLogin = require("../../middlewares/isTeacherLogin");
+const isTeacher = require("../../middlewares/isTeacher");
+
+//@desc Create question
+//@route POST /api/v1/:examId/questions
+//@access Private teachers only
+
+exports.createQuestion = AsyncHandler(async (req, res) => {
+  const { question, optionA, optionB, optionC, optionD, correctAnswer } =
+    req.body;
+
+  //find the exam
+
+  const examFound = await Exam.findById(req.params.examId);
+
+  if (!examFound) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Exam not found",
+    });
+  }
+
+  //create question
+
+  const questionCreated = await Question.create({
+    question,
+    optionA,
+    optionB,
+    optionC,
+    optionD,
+    correctAnswer,
+    createdBy: req.userAuth._id,
+  });
+
+  //add question to the exam
+  examFound.questions.push(questionCreated?._id);
+  //save the exam
+  await examFound.save();
+  res.status(201).json({
+    status: "success",
+    message: "Question created successfully",
+    data: questionCreated,
+  });
+});
