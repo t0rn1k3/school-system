eea0ada451b6ec24cacb63354ea4c748f7ddcbfb
t0rn1k3 <tornikeburjanadze0@gmail.com>
2026-02-13 15:08:00 +0400

    get all exam resutls

--- controller/academic/examResutlCtrl.js
+++ controller/academic/examResutlCtrl.js
@@ -1,4 +1,5 @@
 const AsyncHandler = require("express-async-handler");
+const ExamResult = require("../../model/Academic/ExamResults");
 
 //@desc exam result checking
 //@route GET /api/v1/exam-results/:id
@@ -8,5 +9,18 @@ exports.checkExamResultCtrl = AsyncHandler(async (req, res) => {
   res.status(200).json({
     status: "success",
     message: "Exam result checked successfully",
+  });
+});
+
+//@desc get all exam results
+//@route GET /api/v1/exam-results
+//@access Private
+
+exports.getAllExamResultsCtrl = AsyncHandler(async (req, res) => {
+  const results = await ExamResult.find();
+  res.status(200).json({
+    status: "success",
+    message: "Exam results fetched successfully",
+    data: results,
   });
 });
--- routes/academics/examResultRoute.js
+++ routes/academics/examResultRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   checkExamResultCtrl,
+  getAllExamResultsCtrl,
 } = require("../../controller/academic/examResutlCtrl");
 
 const examResultRouter = express.Router();
 
 examResultRouter.get("/:id", checkExamResultCtrl);
+examResultRouter.get("/", getAllExamResultsCtrl);
 
 module.exports = examResultRouter;
