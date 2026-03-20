9ba2f9136a112d9f99e5d10f78fddbdd80acd39f
t0rn1k3 <tornikeburjanadze0@gmail.com>
2026-03-20 21:43:42 +0400

    correct authCache require paths in nested controllers

--- controller/staff/teachersCtrl.js
+++ controller/staff/teachersCtrl.js
@@ -423,7 +423,7 @@ exports.updateTeacherProfileCtrl = AsyncHandler(async (req, res) => {
     }
 
     const token = req.headers?.authorization?.split(" ")[1];
-    if (token) require("../utils/authCache").del(token);
+    if (token) require("../../utils/authCache").del(token);
 
     return res.status(200).json({
       status: "success",
--- controller/students/studentsCtrl.js
+++ controller/students/studentsCtrl.js
@@ -424,7 +424,7 @@ exports.updateStudentProfileCtrl = AsyncHandler(async (req, res) => {
     }
 
     const token = req.headers?.authorization?.split(" ")[1];
-    if (token) require("../utils/authCache").del(token);
+    if (token) require("../../utils/authCache").del(token);
 
     return res.status(200).json({
       status: "success",
